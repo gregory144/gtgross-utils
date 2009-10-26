3b39b9f7e35ccb9ea85de31a9236f4373490b939
Gregory Gross <greg@gtgross.com>
2009-10-26 18:03:14 -0400

    cleaned up some debug stuff

--- scrim.rb
+++ scrim.rb
@@ -6,23 +6,22 @@ require 'httpclient'
 
 require 'cgi'
 
-url = "http://scr.im/test"
+url = ARGV[0] || "http://scr.im/test"
 found = false
 
-clnt = HTTPClient.new("http://localhost:8888")
+clnt = HTTPClient.new
 
 while (not found) do
 
-    captcha_value = "";
+    captcha_value = nil;
 
     q_doc = Hpricot(clnt.get(url).content)
 
     q_doc.search("//script").each do |x|
         if (x.html =~ /var caps = ([^;]+);/)
-            caps = $1;
-            puts caps;
             captcha_value = [];
-            caps.scan(/"[^"]*"/) { |x| captcha_value.push(CGI.unescape(x[1..-2])); }
+            $1.scan(/"[^"]*"/) { |x| captcha_value.push(CGI.unescape(x[1..-2])); }
+            #pick a random captcha value
             captcha_value = captcha_value[rand(captcha_value.size)];
         end
     end
@@ -35,9 +34,7 @@ while (not found) do
         req_params[:captcha] = captcha_value;
         req_params[:ajax] = "y";
 
-        puts req_params;
-
-        a_doc = Hpricot(clnt.post("http://scr.im/test", req_params).content);
+        a_doc = Hpricot(clnt.post(url, req_params).content);
 
         if (a_doc.search("//p[@id='reveal_mail']").size > 0)
             found = true;
